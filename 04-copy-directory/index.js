0f88c64506fde433910c0728e11452196de24370
Valentina Ivanova <iv.valya.vi85@gmail.com>
2022-11-05 21:39:05 +0300

    feat: create function copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir(path.join(__dirname, 'files-copy'),
+  { recursive: true }, (err) => {
+    if (err) {
+      console.log("Error Found:", err);
+    }
+  });
+  fs.readdir(path.join(__dirname, '/files/'), 'utf-8', (err, files) => {
+    if (err) throw err;
+    files.forEach((item) => {
+      fs.copyFile(path.join(__dirname, '/files/', item), path.join(__dirname, '/files-copy/', item), (err) => {
+        if (err) {
+          console.log("Error Found:", err);
+        };
+      });
+    })
+  });
